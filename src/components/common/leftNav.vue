e4519c89b18ed48318b805863fcdb3a14b7425d8
lilingling <1192246772@qq.com>
2017-07-03 21:45:19 +0800

    'pos'

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="left-nav">
+   <ul>
+        <li>
+            <i class="icon iconfont icon-wodezichan"></i>
+            <div>收银</div>
+        </li>
+ 
+        <li>
+            <i class="icon iconfont icon-dianpu"></i>
+            <div>店铺</div>
+        </li>
+ 
+        <li>
+            <i class="icon iconfont icon-31quanbushangpin"></i>
+            <div>商品</div>
+        </li>
+ 
+         <li>
+            <i class="icon iconfont icon-huiyuanqia"></i>
+            <div>会员</div>
+        </li>
+ 
+ 
+        <li>
+            <i class="icon iconfont icon-zuanshi"></i>
+            <div>统计</div>
+        </li>
+        <li>
+            <i class="icon iconfont icon-shezhi"></i>
+            <div>设置</div>
+        </li>
+</ul>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'leftNav',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+    .left-nav{
+        color: #fff;
+        background-color: #1D8CE0;
+        font-size: 10px;
+        height: 100%;
+        width: 5%;
+        float: left
+    }
+    .iconfont{
+        font-size: 24px
+    }
+    .left-nav ul{
+        margin: 0;
+        padding: 0;
+    }
+
+    .left-nav li{
+        list-style: none;
+        text-align: center;
+        border-bottom: 1px solid #20a0ff;
+        padding: 10px
+    }
+</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <!--<h1>pos page</h1>-->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
